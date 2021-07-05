cf35eeb1213105013cd624268bb776b708400089
soojang <solitasroh@gmail.com>
2021-07-05 23:10:14 +0900

    connector 수정중...

--- src/components/Connector.js
+++ src/components/Connector.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 const { ipcRenderer } = window.require("electron");
 export default function Connector() {
   const payload = "message";
+  const [ip, setIp] = useState("0.0.0.0");
+  const handleIPChange = ({ target: { value } }) => setIp(value);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // ipcRenderer에서의 이벤트 송신
-    ipcRenderer.send("CONNECT", payload);
+    ipcRenderer.send("CONNECT", ip);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="IP" />
+        <input type="text" name="IP" value={ip} onChange={handleIPChange} />
         <input type="submit" value="connect" />
       </form>
     </div>
